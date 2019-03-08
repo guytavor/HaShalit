de6b0518470aa6639fe661944e02bb239f0d6e8a
Or Avrahamy <or.avrahamy@ironsrc.com>
2019-03-08 23:10:40 +0200

    Answers trigger when off center. “Snap” to center to close enough to the center.

--- src/App.js
+++ src/App.js
@@ -34,21 +34,40 @@ function Deck() {
     const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) }))
     // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
     const bind = useGesture(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
-        const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+        // If you flick hard enough it should trigger the card to fly out
+        const isInCenter = Math.abs(xDelta) < 10
+        const isMovedToAnswer = Math.abs(xDelta) > 40
+        const trigger = velocity > 0.2
         const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
-        if (!down) {
+        if (!down || !isMovedToAnswer) {
             setAnswer('');
         } else {
             setAnswer(xDelta > 0 ? 'Do this' : (xDelta === 0 ? '' : 'Do that'));
         }
-        if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+        if (!down && isMovedToAnswer && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
         set(i => {
             if (index !== i) return // We're only interested in changing spring-data for the current spring
             const isGone = gone.has(index)
-            const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
-            const rot = down ? xDelta / 100 + (isGone ? dir * 10 * velocity : 0) : 0// How much the card tilts, flicking it harder makes it rotate faster
-            const scale = down ? 1.1 : 1 // Active cards lift up a bit
-            return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
+            const springData = {
+                x: 0,
+                y: 0,
+                rot: down ? xDelta / 100 : 0,
+                // Active cards lift up a bit
+                scale: down ? 1.05 : 1,
+                config: {friction: 50, tension: down ? 800 : isGone ? 200 : 500}
+            }
+            if (isGone) {
+                // When a card is gone it flys out left or right, otherwise goes back to zero
+                springData.x = (200 + window.innerWidth) * dir
+                springData.y = 200 + window.innerHeight
+                // How much the card tilts, flicking it harder makes it rotate faster
+                springData.rot += dir * 10 * velocity
+            } else if (isInCenter) {
+                springData.rot = 0
+            } else {
+                springData.x = down ? xDelta : 0
+            }
+            return springData
         })
         if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
     })
