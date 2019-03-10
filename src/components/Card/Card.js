9e57e621c8e6af1530b884ae76975854e2c52d14
Or Avrahamy <or.avrahamy@ironsrc.com>
2019-03-10 10:19:09 +0200

    Movement tweaks and comments

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -11,7 +11,7 @@ function Card({ imgUrl, onMoveToSide, onFlip, onRelease }) {
     const [props, set] = useSpring(() => ({ from }));
 
     const actionHandlers = useGesture(({ down, delta: [xDelta] }) => {
-        const isInCenter = Math.abs(xDelta) < 10;
+        const isInCenter = Math.abs(xDelta) < 5;
         const isMovedToAnswer = Math.abs(xDelta) > 20;
 
         const dir = Math.sign(xDelta);
@@ -33,7 +33,8 @@ function Card({ imgUrl, onMoveToSide, onFlip, onRelease }) {
                 y: 0,
                 rot: 0,
                 scale: down ? 1.05 : 1,
-                config: { friction: 45, tension: down ? 800 : gone ? 100 : 500 }
+                // Friction 50 means no bounce at all. Below that increases the bounce.
+                config: { friction: 42, tension: down ? 800 : gone ? 100 : 500 }
             };
 
             if (gone) {
@@ -43,13 +44,26 @@ function Card({ imgUrl, onMoveToSide, onFlip, onRelease }) {
             } else if (down) {
                 springData.y = -10;
                 if (!isInCenter) {
-                    const absDelta = Math.abs(xDelta) * 3;
+                    const dragSensitivity = 2
+                    const absDelta = Math.abs(xDelta);
+                    // This is a Sigmoid function. It slowly increase from 0, then at a
+                    // certain "sweet spot" quickly increases and finally flattens at 1.
+                    // Increase the slope to make the card snap harder when reaching the
+                    // "sweet spot".
                     const slope = 1.08;
+                    // By default, the sweet spot is around x=0. This offset moves it.
                     const xOffset = 50;
-                    const x = dir * 85 / (1 + Math.pow(slope, -(absDelta - xOffset))) + xDelta / 10;
-                    springData.x = Math.max(-100, Math.min(x, 100));
-                    springData.y += springData.x * springData.x / 220;
-                    springData.rot = springData.x * 0.15;
+                    let x = dir * 85 / (1 + Math.pow(slope, -(absDelta * dragSensitivity - xOffset)));
+                    // Add a linear progression to make the flat start and end less flat.
+                    // It improves the feeling of responsiveness.
+                    x += xDelta / 10;
+                    // Clamp the X position so it will stop at the edges and not move too
+                    // far out of the screen.
+                    const stopMovingAtX = 100;
+                    springData.x = Math.max(-stopMovingAtX, Math.min(x, stopMovingAtX));
+                    // Exponentially lower the card as it reaches the edge to create an arc.
+                    springData.y += springData.x * springData.x / 300;
+                    springData.rot = springData.x * 0.1;
                 }
             }
             return springData;
