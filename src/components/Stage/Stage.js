d56ccc34c11decefce73f991cb2ca68d1567169f
inbalvenn <inbal@venn.city>
2019-03-10 21:16:31 +0200

    Seperate answers to left and right

--- src/components/Stage/Stage.js
+++ src/components/Stage/Stage.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Deck from '../Deck/Deck';
-import styles from './Stage.module.scss';
+import React from "react";
+import { connect } from "react-redux";
+import Deck from "../Deck/Deck";
+import styles from "./Stage.module.scss";
 
 class Stage extends React.PureComponent {
-    render() {
-        const { answers, side } = this.props;
-        const answer = answers && side ? answers[side] : '';
+  render() {
+    const { answers, side } = this.props;
+    const answerLeft = answers && (side === 'left') ? answers["left"] : "";
+    const answerRight = answers && (side === 'right') ? answers["right"] : "";
 
-        return (
-            <div className={styles.stage}>
-                <h2 className={styles.answer}>{answer}</h2>
-                <Deck />
-            </div>
-        );
-    }
-};
+    return (
+      <div className={styles.stage}>
+        <div className={styles.answerContainer}>
+            <h2 className={`first ${styles.answer}`}>{answerLeft}</h2>
+            <h2 className={styles.answer}>{answerRight}</h2>
+        </div>
+        <Deck />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
-    return {
-        answers: state.currentCard ? state.currentCard.answers : null,
-        side: state.currentSide,
-    }
+  return {
+    answers: state.currentCard ? state.currentCard.answers : null,
+    side: state.currentSide
+  };
 };
 
 export default connect(mapStateToProps)(Stage);
