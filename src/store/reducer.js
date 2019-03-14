5a07b7d7ea67efedf003740304c58edb62fc7295
Or Avrahamy <or.avrahamy@ironsrc.com>
2019-03-14 23:41:43 +0200

    FIX: Missing definition

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -29,6 +29,7 @@ const INITIAL_STATE = {
         cardsUnlocked: new Set(),
         cardsUnlockedForever: new Set(),
         playedCards: new Set(),
+        boosters: [],
         // TODO: Use the history array to display the timeline.
         history: [
             /*{
