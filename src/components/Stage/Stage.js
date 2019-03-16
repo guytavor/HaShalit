07034d5fed58b21b241389eed7aa19a6d35b88ec
Or Avrahamy <or.avrahamy@ironsrc.com>
2019-03-16 20:33:23 +0200

    Indentation and added default case.

--- src/components/Stage/Stage.js
+++ src/components/Stage/Stage.js
@@ -12,58 +12,60 @@ import { SCREENS } from '../../utils/constants';
 import styles from "./Stage.module.scss";
 
 class Stage extends React.PureComponent {
-  render() {
-    return this._getContent();
-  }
-
-  _getContent() {
-    const { screen, card, interactions, selectAnswer, moveCard, startGame, startOver } = this.props;
-    let content = null;
-
-    switch(screen) {
-      case SCREENS.INTRO:
-        content = <Intro startGame={startGame} />
-        break;
-      case SCREENS.WON:
-        content = <Win startOver={startOver} />
-        break;
-      case SCREENS.LOST:
-        content = <Lost startOver={startOver} />
-        break;
-      case SCREENS.GAME:
-      case SCREENS.CREDITS:
-        content = (
-          <React.Fragment>
-            <Header />
-            <DeckContainer card={card} 
-                           interactions={interactions}
-                           onSelectAnswer={selectAnswer}
-                           onMoveCard={moveCard} />
-            <Footer />
-          </React.Fragment>
-        );
-        break;
+    render() {
+        return this._getContent();
     }
 
-    return <div className={styles.stage}>{content}</div>;
-  }
+    _getContent() {
+        const { screen, card, interactions, selectAnswer, moveCard, startGame, startOver } = this.props;
+        let content = null;
+
+        switch(screen) {
+            case SCREENS.INTRO:
+                content = <Intro startGame={startGame} />
+                break;
+            case SCREENS.WON:
+                content = <Win startOver={startOver} />
+                break;
+            case SCREENS.LOST:
+                content = <Lost startOver={startOver} />
+                break;
+            case SCREENS.GAME:
+            case SCREENS.CREDITS:
+                content = (
+                    <React.Fragment>
+                        <Header />
+                        <DeckContainer card={card} 
+                                                     interactions={interactions}
+                                                     onSelectAnswer={selectAnswer}
+                                                     onMoveCard={moveCard} />
+                        <Footer />
+                    </React.Fragment>
+                );
+                break;
+            default:
+                break;
+        }
+
+        return <div className={styles.stage}>{content}</div>;
+    }
 }
 
 const mapStateToProps = state => {
-  return {
-      screen: state.screen,
-      card: state.level.card,
-      interactions: state.interactions
-  };
+    return {
+        screen: state.screen,
+        card: state.level.card,
+        interactions: state.interactions
+    };
 };
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-      selectAnswer: actions.selectAnswer,
-      moveCard: actions.moveCard,
-      startGame: actions.startGame,
-      startOver: actions.startOver,
-  }, dispatch);
+    return bindActionCreators({
+        selectAnswer: actions.selectAnswer,
+        moveCard: actions.moveCard,
+        startGame: actions.startGame,
+        startOver: actions.startOver,
+    }, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Stage);
