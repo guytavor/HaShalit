8cd35a5cc6250404268bb83f9b749a24d888ce28
inbalvenn <inbal@venn.city>
2019-03-16 09:43:58 +0200

    add effects icons to view

--- src/components/Effects/Effects.js
+++ src/components/Effects/Effects.js
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./Effects.module.scss";
 import ReligionIcon from "./Icons/ReligionIcon"
+import ReligionIconInverse from "./Icons/ReligionIconInverse"
+import EconomyIcon from "./Icons/EconomyIcon"
+import EconomyIconInverse from "./Icons/EconomyIconInverse"
 
 function Effects() {
-  return (
+
+	return (
 		<div className={styles.effectsContainer}>
      <div className={styles.iconContainer}>
 			<div className={`${styles.icon} ${styles.iconBg}`}>
@@ -13,6 +17,34 @@ function Effects() {
 				<ReligionIcon className={`${styles.Icon} ${styles.iconFill}`}/>
 			</div>
 		 </div>
+
+		 <div className={styles.iconContainer}>
+			<div className={`${styles.icon} ${styles.iconBg}`}>
+				<EconomyIcon className={`${styles.Icon} ${styles.iconBg}`}/>
+			</div>
+			<div className={`${styles.icon} ${styles.iconFill}`}>
+				<EconomyIcon className={`${styles.Icon} ${styles.iconFill}`}/>
+			</div>
+		 </div>
+
+		 <div className={styles.iconContainer}>
+			<div className={`${styles.icon} ${styles.iconBg}`}>
+				<ReligionIconInverse className={`${styles.Icon} ${styles.iconBg}`}/>
+			</div>
+			<div className={`${styles.icon} ${styles.iconFill}`}>
+				<ReligionIconInverse className={`${styles.Icon} ${styles.iconFill}`}/>
+			</div>
+		 </div>
+
+		 <div className={styles.iconContainer}>
+			<div className={`${styles.icon} ${styles.iconBg}`}>
+				<EconomyIconInverse className={`${styles.Icon} ${styles.iconBg}`}/>
+			</div>
+			<div className={`${styles.icon} ${styles.iconFill}`}>
+				<EconomyIconInverse className={`${styles.Icon} ${styles.iconFill}`}/>
+			</div>
+		 </div>
+
 		</div>
   );
 }
