317e8690ba75ab3a07a0f70ac0461d68beb699d6
jonathanadler <jonathanadler2@gmail.com>
2019-03-18 10:31:09 +0200

    allow fullscreen mode

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.module.scss';
 import App from './components/App';
 
+try {
+    window.scrollTo(0,1);
+} catch (e) {}
+
 ReactDOM.render(<App />, document.getElementById('root'));
