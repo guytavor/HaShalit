d7b1df7e523f6f5cf9677a078a7084aaa4fb55fc
jonathanadler <jonathanadler2@gmail.com>
2019-03-24 22:03:52 +0200

    import fonts

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.module.scss';
 import App from './components/App';
+import './fonts.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
