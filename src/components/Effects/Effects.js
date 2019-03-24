fdc0463a6ccb41632043c35cb74f701b234b398f
jonathanadler <jonathanadler2@gmail.com>
2019-03-24 23:35:33 +0200

    colors

--- src/components/Effects/Effects.js
+++ src/components/Effects/Effects.js
@@ -5,28 +5,29 @@ import SecurityIcon from "./Icons/SecurityIcon";
 import ImageIcon from "./Icons/ImageIcon";
 import Effect from "../Effect/Effect";
 import styles from "./Effects.module.scss";
+import colors from "../../common.scss";
 
 function Effects({ points, willEffect = {} }) {
   return (
     <div className={styles.effectsContainer}>
       <div className={styles.iconContainer}>
         <Effect points={points.religion} willEffect={willEffect.religion}>
-          <ReligionIcon />
+          <ReligionIcon color={colors.stripColor} />
         </Effect>
       </div>
       <div className={styles.iconContainer}>
         <Effect points={points.image} willEffect={willEffect.image}>
-          <ImageIcon />
+          <ImageIcon color={colors.stripColor} />
         </Effect>
       </div>
       <div className={styles.iconContainer}>
         <Effect points={points.security} willEffect={willEffect.security}>
-          <SecurityIcon />
+          <SecurityIcon color={colors.stripColor} />
         </Effect>
       </div>
       <div className={styles.iconContainer}>
         <Effect points={points.economy} willEffect={willEffect.economy}>
-          <EconomyIcon />
+          <EconomyIcon color={colors.stripColor} />
         </Effect>
       </div>
     </div>
