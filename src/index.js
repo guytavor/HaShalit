1b24259719eed6cb55e9953b6edfea17314d964d
jonathanadler <jonathanadler2@gmail.com>
2019-03-26 20:39:51 +0200

    add static page for seo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.module.scss';
-import App from './components/App';
-import './fonts.css';
+import React from "react";
+import { hydrate, render } from "react-dom";
+import "./index.module.scss";
+import App from "./components/App";
+import "./fonts.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
