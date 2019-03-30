cd46fe31d9cb24429e127eb82f45ff70d3d12f9b
jonathanadler <jonathanadler2@gmail.com>
2019-03-31 00:33:02 +0300

    make google analytics work

--- src/utils/AnalyticsHelper.js
+++ src/utils/AnalyticsHelper.js
@@ -1,27 +1,32 @@
-import { err } from '../utils/Logger';
+import { err } from "../utils/Logger";
 
 let send = () => {};
 
 const categories = {
-    SCREEN: 'screen',
-    CARD: 'card'
+  SCREEN: "screen",
+  CARD: "card"
 };
 
 export const CATEGORIES = Object.keys(categories).reduce((acc, key) => {
-    acc[key] = key;
-    return acc;
+  acc[key] = key;
+  return acc;
 }, {});
 
-if (typeof window.ga === 'function') {
-    send = function (category, event, label, value) {
-        try {
-            if (categories[category]) {
-                window.ga('send', 'event', categories[category], event, label, value);
-            }
-        } catch (e) {
-            err('Failed to send analytics', e);
+if (typeof window.ga === "function") {
+  try {
+    const tracker = window.ga.getAll()[0];
+    send = function(category, event, label, value) {
+      try {
+        if (categories[category]) {
+            tracker.send("event", categories[category], event, label, value);
         }
-    }
+      } catch (e) {
+        err("Failed to send analytics", e);
+      }
+    };
+  } catch (e) {
+    err(e);
+  }
 }
 
 export const sendEvent = send;
